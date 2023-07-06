19696239c51b453ef15c6553d3975af8ff14154b
Nikita Levushkin <therederickmind@gmail.com>
2023-07-06 15:13:24 +0300

    Sprint2 B solution for linked lists added

--- python/sprint2/B/code.py
+++ python/sprint2/B/code.py
@@ -1,18 +1,18 @@
 # ! change LOCAL to False before submitting !
 # set LOCAL to True for local testing
 
-LOCAL = False
 
-if LOCAL:
-    class Node:  
-        def __init__(self, value, next_item=None):  
-            self.value = value  
-            self.next_item = next_item
+class Node:
+    def __init__(self, value, next_item=None):
+        self.value = value
+        self.next_item = next_item
+
 
 def solution(node):
-    # Your code
-    # ヽ(´▽`)/
-    pass
+    while node:
+        print(node.value)
+        node = node.next_item
+
 
 def test():
     node3 = Node("node3", None)
@@ -26,6 +26,6 @@ def test():
     # node2
     # node3
 
+
 if __name__ == '__main__':
     test()
-    
